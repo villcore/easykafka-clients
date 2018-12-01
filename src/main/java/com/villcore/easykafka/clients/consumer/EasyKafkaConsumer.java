9a5e7a1f06eeb37d308eabfc5ff4a0f18991a771
villcore <villcore@163.com>
2018-12-01 22:25:05 +0800

    update.

---
+++ src/main/java/com/villcore/easykafka/clients/consumer/EasyKafkaConsumer.java
@@ -0,0 +1,8 @@
+package com.villcore.easykafka.clients.consumer;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+@ThreadSafe
+public interface EasyKafkaConsumer {
+
+}
--- src/main/java/com/villcore/easykafka/clients/producer/EasyKafkaProducerImpl.java
+++ src/main/java/com/villcore/easykafka/clients/producer/EasyKafkaProducerImpl.java
@@ -1,7 +1,5 @@
 package com.villcore.easykafka.clients.producer;
 
-import com.villcore.easykafka.clients.SendCallback;
-import com.villcore.easykafka.clients.SendResult;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,64 +20,63 @@ public class EasyKafkaProducerImpl<K, V> implements com.villcore.easykafka.clien
         producer = new KafkaProducer<byte[], byte[]>(prop);
     }
 
-
     @Override
     public SendResult sendSync(String topic, V value) {
         return null;
     }
 
     @Override
-    public com.villcore.easykafka.clients.producer.SendResult sendSync(String topic, K key, V value) {
+    public SendResult sendSync(String topic, K key, V value) {
         return null;
     }
 
     @Override
-    public com.villcore.easykafka.clients.producer.SendResult sendSync(String topic, int partition, K key, V value) {
+    public SendResult sendSync(String topic, int partition, K key, V value) {
         return null;
     }
 
     @Override
-    public com.villcore.easykafka.clients.producer.SendResult sendSync(String topic, V value, Map<String, Object> header) {
+    public SendResult sendSync(String topic, V value, Map<String, Object> header) {
         return null;
     }
 
     @Override
-    public com.villcore.easykafka.clients.producer.SendResult sendSync(String topic, K key, V value, Map<String, Object> header) {
+    public SendResult sendSync(String topic, K key, V value, Map<String, Object> header) {
         return null;
     }
 
     @Override
-    public com.villcore.easykafka.clients.producer.SendResult sendSync(String topic, int partition, K key, V value, Map<String, Object> header) {
+    public SendResult sendSync(String topic, int partition, K key, V value, Map<String, Object> header) {
         return null;
     }
 
     @Override
-    public Future<com.villcore.easykafka.clients.producer.SendResult> sendAsync(String topic, V value, SendCallback sendCallback) {
+    public Future<SendResult> sendAsync(String topic, V value, SendCallback sendCallback) {
         return null;
     }
 
     @Override
-    public Future<com.villcore.easykafka.clients.producer.SendResult> sendAsync(String topic, K key, V value, com.villcore.easykafka.clients.producer.SendCallback sendCallback) {
+    public Future<SendResult> sendAsync(String topic, K key, V value, SendCallback sendCallback) {
         return null;
     }
 
     @Override
-    public Future<com.villcore.easykafka.clients.producer.SendResult> sendAsync(String topic, int partition, K key, V value, com.villcore.easykafka.clients.producer.SendCallback sendCallback) {
+    public Future<SendResult> sendAsync(String topic, int partition, K key, V value, SendCallback sendCallback) {
         return null;
     }
 
     @Override
-    public Future<com.villcore.easykafka.clients.producer.SendResult> sendAsync(String topic, V value, Map<String, Object> header, com.villcore.easykafka.clients.producer.SendCallback sendCallback) {
+    public Future<SendResult> sendAsync(String topic, V value, Map<String, Object> header, SendCallback sendCallback) {
         return null;
     }
 
     @Override
-    public Future<com.villcore.easykafka.clients.producer.SendResult> sendAsync(String topic, K key, V value, Map<String, Object> header, com.villcore.easykafka.clients.producer.SendCallback sendCallback) {
+    public Future<SendResult> sendAsync(String topic, K key, V value, Map<String, Object> header, SendCallback sendCallback) {
         return null;
     }
 
     @Override
-    public Future<com.villcore.easykafka.clients.producer.SendResult> sendAsync(String topic, int partition, K key, V value, Map<String, Object> header, com.villcore.easykafka.clients.producer.SendCallback sendCallback) {
+    public Future<SendResult> sendAsync(String topic, int partition, K key, V value, Map<String, Object> header, SendCallback sendCallback) {
         return null;
     }
 
